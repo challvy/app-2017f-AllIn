8deb3eec7a6aa6618bb5995373cb46e7b6e03862
Xxxxxzr <715534986@qq.com>
2017-12-04 15:28:10 +0800

    Add cell

---
+++ AllIn/AllIn/ContentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContentTableViewCell.swift
+//  AllIn
+//
+//  Created by apple on 2017/12/4.
+//
+
+import UIKit
+
+class ContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
